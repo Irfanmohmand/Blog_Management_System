424f0e105f2c88d4c765a2e19130db6203ca6fc5
Irfan Mohmand <irfanmohmand987@gmail.com>
2025-03-03 06:38:21 +0500

    Fixed missing dependencies

--- server.js
+++ server.js
@@ -38,6 +38,8 @@ app.use("/api/blog", blogRouter);
 
 
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server is running on port ${process.env.PORT}`)
-})
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
